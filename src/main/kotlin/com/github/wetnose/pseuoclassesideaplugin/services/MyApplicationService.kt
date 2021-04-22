e8fda0fbafa047d8c96a879b29c36f9ee97a9155
GitHub Action <action@github.com>
2021-04-22 13:49:41 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/wetnose/pseuoclassesideaplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.wetnose.pseuoclassesideaplugin.services
+
+import com.github.wetnose.pseuoclassesideaplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
